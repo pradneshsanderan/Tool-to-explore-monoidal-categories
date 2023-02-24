ac0259c6dfc241b21fa1bf9be40a0b826bdf6d41
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2023-02-24 22:00:59 +0000

    added structures

--- FYP/src/Main.java
+++ FYP/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /**
+         * TODO
+         * make a new class to represent the graph
+         * add functions to the graph so its easier to check associativity
+         * functions neeeded:
+         *  getStates()
+         *  getMorphisms()
+         *  getNextState(morphism m)
+         *  getIncomingMorphisms(state s)
+         *  getOutgoingMorphisms(state s)
+         *  getIdentity(state s)
+         *  checkAssociativity(state a, state b, state c)
+         *
+         */
+
+        /**\
+         * DATA STRUCTURE NOTES
+         * bulid a new class to represent the graphs
+         * the class will have states and morphisms connecting the states
+         * option 1 use a hashmap. states as keys and a list of morphisms in the graph as values
+         * issue: how to know if the state is incoming or outgoing? and how to know which state it goes to?
+         *
+         */
+
     }
 }
