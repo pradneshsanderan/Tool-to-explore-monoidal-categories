f4a81a7f7141e08e32a653cf4692af8750d3665f
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2023-02-28 20:21:16 +0000

    added monoidal description

--- FYP/src/Main.java
+++ FYP/src/Main.java
@@ -17,10 +17,7 @@ public class Main {
 
     /**
      * TODO
-     *
-     *  check if each state has an identity
-     *  cycle through the morphisms just like in the table and check that they all follow the associativity law
-     *
+     *Add java docs for each class and method in each class
      *
      */
 
@@ -30,6 +27,47 @@ public class Main {
      * first list all states, then followed by a morphism followed by 2 states
      * Eg:
      * A1,A2,A3, f1,A1,A2, f2,A2,A2,f3,A2,A3
+     */
+
+
+    /**
+     * TODO: THE MONOIDAL PART
+     * A monoidal category is a category equipped with a tensor product and a unit object, subject to the following properties:
+     *
+     *      1)Associativity: For any objects A, B, and C in the category, there is a natural isomorphism (A ⊗ B) ⊗ C ≅ A ⊗ (B ⊗ C).
+     *
+     *      2)Unit: There exists a unit object I such that for any object A in the category, there are natural isomorphisms A ⊗ I ≅ A and I ⊗ A ≅ A.
+     *
+     *      3)Coherence: The associativity and unit isomorphisms are required to satisfy certain coherence conditions involving
+     *      higher-order associativity and commutativity isomorphisms.
+     *
+     *      4)Compatibility with functors: Given two monoidal categories (C, ⊗, I) and (D, ⊙, J),
+     *      a monoidal functor F : C → D is a functor that preserves the tensor product and unit object,
+     *      i.e. F(A ⊗ B) = F(A) ⊙ F(B) and F(I) = J, and also preserves the coherence isomorphisms.
+     *
+     *      5)Symmetry (for a symmetric monoidal category): There is a natural isomorphism A ⊗ B ≅ B ⊗ A, for any objects A and B.
+     *
+     *      6)Braiding (for a braided monoidal category): There is a natural isomorphism A ⊗ B ≅ B ⊗ A, together with coherence conditions.
+     *
+     *      7)Duals (for a monoidal category with duals): For any object A in the category, there exist objects A^* and εA,
+     *      together with natural isomorphisms A ⊗ A^* ≅ I and εA ⊗ A ≅ I, where I is the unit object,
+     *      such that A^* is the dual of A and εA is the counit of A. The dual and counit satisfy certain coherence conditions
+     *
+     *
+     *
+     * In a monoidal category, the tensor product is a bifunctor, which means that it takes two objects and returns another object.
+     * So, to check if a monoidal category has a bifunctor, we need to check if the tensor product satisfies the following conditions:
+     *
+     *      1) It is a functor in each variable separately, meaning that for any two morphisms f: A → A' and g: B → B' in the category,
+     *      there is a corresponding morphism f ⊗ g: A ⊗ B → A' ⊗ B' in the category.
+     *
+     *      2)It is associative up to natural isomorphism, meaning that there is a natural isomorphism associating
+     *      ((A ⊗ B) ⊗ C) with (A ⊗ (B ⊗ C)) for all objects A, B, and C in the category.
+     *
+     *      3) It has a unit object I, such that there is a natural isomorphism between (A ⊗ I) and A,
+     *      and between (I ⊗ A) and A, for all objects A in the category.
+     *
+     *
      */
     public static void main(String[] args) {
         readFile();
